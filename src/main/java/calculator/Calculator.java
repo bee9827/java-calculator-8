6a37f0d871635e1eee94afd5e1829996df96e12f
bee9827 <ehfrhfo9494@naver.com>
2025-10-15 21:28:55 +0900

    fix: 양수만 받도록 수정

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -6,14 +6,21 @@ public class Calculator {
     private Calculator() {
     }
 
-    public static int addNumbers(List<Integer> numbers) {
+    public static int addPositiveNumbers(List<Integer> numbers) {
+        numbers.forEach(Calculator::validatePositive);
         return numbers.stream()
                 .reduce(Integer::sum)
                 .orElse(0);
     }
 
-    public static int addStringNumbers(List<String> numbers) {
-        return addNumbers(toIntegerNumbers(numbers));
+    private static void validatePositive(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("양수를 입력해 주세요");
+        }
+    }
+
+    public static int addPositiveStringNumbers(List<String> numbers) {
+        return addPositiveNumbers(toIntegerNumbers(numbers));
     }
 
     private static List<Integer> toIntegerNumbers(List<String> numbers) {
